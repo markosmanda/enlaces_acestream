78a47ce0c222c264da12460ba1bf9d2447da6ce9
markosmanda <115448121+markosmanda@users.noreply.github.com>
2023-10-20 14:29:56 +0200

    4

--- transform2.py
+++ transform2.py
@@ -1,32 +1,21 @@
 import json
 
-# Define la ruta del archivo JSON de entrada y el archivo M3U de salida
-archivo_json = 'channels.json'
-archivo_m3u = 'canales.m3u'
+# Carga el JSON desde el archivo channels.json
+with open('channels.json', 'r', encoding='utf-8') as json_file:
+    data = json.load(json_file)
 
-# Función para cargar el JSON y crear el archivo M3U
-def json_a_m3u(archivo_json, archivo_m3u):
-    try:
-        # Abre el archivo JSON
-        with open(archivo_json, 'r') as json_file:
-            data = json.load(json_file)
-            
-        # Abre el archivo M3U para escritura
-        with open(archivo_m3u, 'w') as m3u_file:
-            # Escribe la cabecera M3U
-            m3u_file.write("#EXTM3U\n")
-            
-            # Recorre los canales y escribe las entradas M3U
-            for canal in data:
-                nombre = canal["Name"]
-                url = canal["Link"]
-                image = canal["Image"]
-                
-                m3u_file.write(f"#EXTINF:-1,{nombre}\n{url}\n{image}")
-                
-        print(f'Archivo M3U creado: {archivo_m3u}')
-    except Exception as e:
-        print(f'Ocurrió un error: {e}')
+# Función para generar una línea M3U para un canal
+def generate_m3u_entry(channel):
+    tvg_logo = channel["Image"]
+    tvg_id = channel["Name"]
+    stream_url = channel["Link"]
+    return f'#EXTINF:-1 tvg-logo="{tvg_logo}" tvg-id="{tvg_id}",{tvg_id}\n{stream_url}\n'
 
-# Llama a la función para convertir JSON a M3U
-json_a_m3u(archivo_json, archivo_m3u)
+# Genera las líneas M3U para todos los canales
+m3u_entries = [generate_m3u_entry(channel) for channel in data]
+
+# Guarda las líneas M3U en un archivo
+with open('canales.m3u', 'w', encoding='utf-8') as m3u_file:
+    m3u_file.write('\n'.join(m3u_entries))
+
+print('Archivo M3U generado con éxito.')
